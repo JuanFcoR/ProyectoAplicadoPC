acee95f121966faede815e5f5fec09226bdab5ec
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-06-14 15:35:33 -0400

    algo

--- UI/Registros/rProducto.cs
+++ UI/Registros/rProducto.cs
@@ -16,5 +16,10 @@ namespace ProyectoAplicadoPC.UI.Registros
         {
             InitializeComponent();
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
