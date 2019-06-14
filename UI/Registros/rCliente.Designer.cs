b6e7e7e2504cbb5e0a0c5a5ec4bb5def5c0c0f3a
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-06-14 15:16:02 -0400

    papa

--- UI/Registros/rCliente.Designer.cs
+++ UI/Registros/rCliente.Designer.cs
@@ -85,7 +85,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(21, 79);
+            this.label2.Location = new System.Drawing.Point(18, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 15);
             this.label2.TabIndex = 3;
