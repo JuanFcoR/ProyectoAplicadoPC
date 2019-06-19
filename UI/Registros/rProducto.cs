07a1ca721e391d0992f36e7490c5d054f3e7761f
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-06-19 00:38:24 -0400

    algunos cambios

--- UI/Registros/rProducto.cs
+++ UI/Registros/rProducto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoAplicadoPC.BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,11 @@ namespace ProyectoAplicadoPC.UI.Registros
         public rProducto()
         {
             InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
