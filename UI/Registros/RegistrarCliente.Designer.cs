ec84dfd90d6c77944ac7f5c7067e6353da5b4ab2
Rosy <Rosy@DESKTOP-HI3GI4E>
2019-06-19 00:35:24 -0400

    pan

---
+++ UI/Registros/RegistrarCliente.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace ProyectoAplicadoPC.UI.Registros
+{
+    partial class RegistrarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Cancelarbutton = new System.Windows.Forms.Button();
+            this.LimiteDeCreditomaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.TelefonoDeReferenciamaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.DirecciontextBox = new System.Windows.Forms.TextBox();
+            this.CelularmaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.TelefonomaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.CedulamaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.ApellidotextBox = new System.Windows.Forms.TextBox();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.CodigonumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.FechadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CodigonumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Guardarbutton.Location = new System.Drawing.Point(154, 381);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(89, 39);
+            this.Guardarbutton.TabIndex = 43;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Cancelarbutton
+            // 
+            this.Cancelarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Cancelarbutton.Location = new System.Drawing.Point(41, 381);
+            this.Cancelarbutton.Name = "Cancelarbutton";
+            this.Cancelarbutton.Size = new System.Drawing.Size(86, 39);
+            this.Cancelarbutton.TabIndex = 42;
+            this.Cancelarbutton.Text = "Cancelar";
+            this.Cancelarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Cancelarbutton.UseVisualStyleBackColor = true;
+            // 
+            // LimiteDeCreditomaskedTextBox
+            // 
+            this.LimiteDeCreditomaskedTextBox.Location = new System.Drawing.Point(143, 326);
+            this.LimiteDeCreditomaskedTextBox.Mask = "99999";
+            this.LimiteDeCreditomaskedTextBox.Name = "LimiteDeCreditomaskedTextBox";
+            this.LimiteDeCreditomaskedTextBox.Size = new System.Drawing.Size(86, 20);
+            this.LimiteDeCreditomaskedTextBox.TabIndex = 41;
+            this.LimiteDeCreditomaskedTextBox.ValidatingType = typeof(int);
+            // 
+            // TelefonoDeReferenciamaskedTextBox
+            // 
+            this.TelefonoDeReferenciamaskedTextBox.Location = new System.Drawing.Point(143, 288);
+            this.TelefonoDeReferenciamaskedTextBox.Mask = "000-000-0000";
+            this.TelefonoDeReferenciamaskedTextBox.Name = "TelefonoDeReferenciamaskedTextBox";
+            this.TelefonoDeReferenciamaskedTextBox.Size = new System.Drawing.Size(86, 20);
+            this.TelefonoDeReferenciamaskedTextBox.TabIndex = 40;
+            // 
+            // DirecciontextBox
+            // 
+            this.DirecciontextBox.Location = new System.Drawing.Point(143, 254);
+            this.DirecciontextBox.Name = "DirecciontextBox";
+            this.DirecciontextBox.Size = new System.Drawing.Size(100, 20);
+            this.DirecciontextBox.TabIndex = 39;
+            // 
+            // CelularmaskedTextBox
+            // 
+            this.CelularmaskedTextBox.Location = new System.Drawing.Point(143, 218);
+            this.CelularmaskedTextBox.Mask = "000-000-0000";
+            this.CelularmaskedTextBox.Name = "CelularmaskedTextBox";
+            this.CelularmaskedTextBox.Size = new System.Drawing.Size(86, 20);
+            this.CelularmaskedTextBox.TabIndex = 38;
+            // 
+            // TelefonomaskedTextBox
+            // 
+            this.TelefonomaskedTextBox.Location = new System.Drawing.Point(143, 183);
+            this.TelefonomaskedTextBox.Mask = "000-000-0000";
+            this.TelefonomaskedTextBox.Name = "TelefonomaskedTextBox";
+            this.TelefonomaskedTextBox.Size = new System.Drawing.Size(86, 20);
+            this.TelefonomaskedTextBox.TabIndex = 37;
+            // 
+            // CedulamaskedTextBox
+            // 
+            this.CedulamaskedTextBox.Location = new System.Drawing.Point(143, 151);
+            this.CedulamaskedTextBox.Mask = "0-0000000-0";
+            this.CedulamaskedTextBox.Name = "CedulamaskedTextBox";
+            this.CedulamaskedTextBox.Size = new System.Drawing.Size(86, 20);
+            this.CedulamaskedTextBox.TabIndex = 36;
+            // 
+            // ApellidotextBox
+            // 
+            this.ApellidotextBox.Location = new System.Drawing.Point(143, 119);
+            this.ApellidotextBox.Name = "ApellidotextBox";
+            this.ApellidotextBox.Size = new System.Drawing.Size(100, 20);
+            this.ApellidotextBox.TabIndex = 35;
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(143, 86);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombretextBox.TabIndex = 34;
+            // 
+            // CodigonumericUpDown
+            // 
+            this.CodigonumericUpDown.Location = new System.Drawing.Point(143, 20);
+            this.CodigonumericUpDown.Name = "CodigonumericUpDown";
+            this.CodigonumericUpDown.Size = new System.Drawing.Size(86, 20);
+            this.CodigonumericUpDown.TabIndex = 33;
+            // 
+            // FechadateTimePicker
+            // 
+            this.FechadateTimePicker.CustomFormat = "";
+            this.FechadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechadateTimePicker.Location = new System.Drawing.Point(143, 53);
+            this.FechadateTimePicker.Name = "FechadateTimePicker";
+            this.FechadateTimePicker.Size = new System.Drawing.Size(86, 20);
+            this.FechadateTimePicker.TabIndex = 32;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 326);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 13);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "LimiteDeCredito";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 288);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "TelefonoDeReferencia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Direccion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Celular";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Cedula";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Fecha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Codigo";
+            // 
+            // RegistrarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 450);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Cancelarbutton);
+            this.Controls.Add(this.LimiteDeCreditomaskedTextBox);
+            this.Controls.Add(this.TelefonoDeReferenciamaskedTextBox);
+            this.Controls.Add(this.DirecciontextBox);
+            this.Controls.Add(this.CelularmaskedTextBox);
+            this.Controls.Add(this.TelefonomaskedTextBox);
+            this.Controls.Add(this.CedulamaskedTextBox);
+            this.Controls.Add(this.ApellidotextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.CodigonumericUpDown);
+            this.Controls.Add(this.FechadateTimePicker);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistrarCliente";
+            this.Text = "RegistrarCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.CodigonumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Cancelarbutton;
+        private System.Windows.Forms.MaskedTextBox LimiteDeCreditomaskedTextBox;
+        private System.Windows.Forms.MaskedTextBox TelefonoDeReferenciamaskedTextBox;
+        private System.Windows.Forms.TextBox DirecciontextBox;
+        private System.Windows.Forms.MaskedTextBox CelularmaskedTextBox;
+        private System.Windows.Forms.MaskedTextBox TelefonomaskedTextBox;
+        private System.Windows.Forms.MaskedTextBox CedulamaskedTextBox;
+        private System.Windows.Forms.TextBox ApellidotextBox;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.NumericUpDown CodigonumericUpDown;
+        private System.Windows.Forms.DateTimePicker FechadateTimePicker;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
